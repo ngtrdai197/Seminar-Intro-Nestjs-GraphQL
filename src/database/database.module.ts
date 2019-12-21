e4d363504c8989984e719c7d3363a38eb4d53a07
Nguyen Dai.dev <ngtrdai290197@gmail.com>
2019-12-22 00:49:27 +0700

    update: add config for MongooseconfigService

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common'
 import { databaseProviders } from './database.providers'
 import { MongooseconfigService } from './mongooseconfig.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '../config/config.module'
+import { ConfigService } from '../config/config.service'
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
+      imports: [ConfigModule],
       useClass: MongooseconfigService, // TODO: useClass
+      inject: [ConfigService],
     }),
   ],
-  providers: [...databaseProviders], // TODO: using for provider
-  exports: [...databaseProviders], // TODO: using for provider
+  // providers: [...databaseProviders], // TODO: using for provider
+  // exports: [...databaseProviders], // TODO: using for provider
 })
 export class DatabaseModule {}
--- src/database/mongooseconfig.service.ts
+++ src/database/mongooseconfig.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { MongooseOptionsFactory, MongooseModuleOptions } from '@nestjs/mongoose'
+import { ConfigService } from '../config/config.service'
 
 @Injectable()
 export class MongooseconfigService implements MongooseOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
   createMongooseOptions(): MongooseModuleOptions {
     return {
-      uri: 'mongodb://localhost/nest',
+      uri: `${this.configService.uriConnectDB}`,
     }
   }
 }
