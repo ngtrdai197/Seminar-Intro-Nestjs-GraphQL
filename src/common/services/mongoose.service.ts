7342c6683e17be572783bb2501fc2560fdcd059a
Nguyen Dai.dev <ngtrdai290197@gmail.com>
2020-02-15 22:00:54 +0700

    update connection db with docker-compose on Ubuntu

--- src/common/services/mongoose.service.ts
+++ src/common/services/mongoose.service.ts
@@ -7,8 +7,9 @@ export class MongooseConfigService implements MongooseOptionsFactory {
   constructor(private configService: ConfigService) {}
   createMongooseOptions(): MongooseModuleOptions {
     return {
-      //   uri: this.configService.get<string>('DB_CONNECTION_STRING'),
-      uri: 'mongodb://localhost/book-store',
+      // TODO: check it, why not able connect to db when use configService
+      // uri: this.configService.get<string>('DB_CONNECTION_STRING'),
+      uri: `mongodb://nest-mongodb:27017/book-store`,
       useNewUrlParser: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
